3c80d79346b91ea66bfc852252767c1e6a865220
jessymatta <jessy.w.matta@gmail.com>
2022-10-15 23:21:49 +0300

    Bckend <> Users Login : imported the jsonwebtoken library

--- linkedin_server/controllers/AuthController.js
+++ linkedin_server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const loginPersonal = (req, res) => {
     res.send("Login Personal");
