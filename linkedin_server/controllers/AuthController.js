585b97bed6a2b8e9d0561e0df6dca0abc4caae5a
jessymatta <jessy.w.matta@gmail.com>
2022-10-15 23:51:57 +0300

    Backend <> Users Login : added the login function in the AuthController

--- linkedin_server/controllers/AuthController.js
+++ linkedin_server/controllers/AuthController.js
@@ -2,8 +2,20 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const loginPersonal = (req, res) => {
-    res.send("Login Personal");
+const loginPersonal = async (req, res) => {
+    const {email, password} = req.body;
+    
+    const user = await User.findOne({email}).lean();
+
+    if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+    const token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '1h'
+    });
+    res.status(200).json(token)
 }
 
 const signupPersonal = async (req, res) => {
