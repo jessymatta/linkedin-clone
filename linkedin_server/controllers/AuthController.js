ddd6ce46ccb46b9ba0f80d926ac518f6ab457bfe
jessymatta <jessy.w.matta@gmail.com>
2022-10-15 23:16:37 +0300

    Backend <> User Login : added the personal account signup function in the AuthController

---
+++ linkedin_server/controllers/AuthController.js
@@ -0,0 +1,30 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const loginPersonal = (req, res) => {
+    res.send("Login Personal");
+}
+
+const signupPersonal = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        res.json(user)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+
+    res.send("SignUp Personal");
+}
+
+module.exports = {
+    loginPersonal,
+    signupPersonal
+}
