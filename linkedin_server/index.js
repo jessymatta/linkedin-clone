6789549d63a68f1fd1c7d80514d34943d12fcc4a
jessymatta <jessy.w.matta@gmail.com>
2022-10-15 22:35:29 +0300

    Backend <> Login/Signup : added the auth routes to index.js

--- linkedin_server/index.js
+++ linkedin_server/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.json());
 require('./config/db.config');
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes)
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
