662d1ae242154099da2fa46c913e3ac68be413e8
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 19:47:38 +0300

    Frontend <> Company homepage : finalizedthe navbar

--- linkedin_frontend/src/components/company_navbar/CompanyNav.js
+++ linkedin_frontend/src/components/company_navbar/CompanyNav.js
@@ -1,10 +1,20 @@
 import "./companynav.css"
 import small_logo from "../../assets/small_logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const CompanyNav = () => {
     return (
         <div className="company_nav">
             <img className="small-logo" src={small_logo} alt="logo"/>
+            <div className="actions">
+            <FontAwesomeIcon className="sidebar-icon" icon={faSuitcase}/>
+            <FontAwesomeIcon className="sidebar-icon" icon={faUser}/>
+            <FontAwesomeIcon className="sidebar-icon" icon={faRightFromBracket}/>
+            </div>
+            
         </div>
     )
 }
