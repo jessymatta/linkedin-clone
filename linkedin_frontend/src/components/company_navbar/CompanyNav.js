e25cc613cf23717fae4bffff09e5ba04e3623a22
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 19:28:24 +0300

    Frontend <> Company Homepage: added logo to navbar

--- linkedin_frontend/src/components/company_navbar/CompanyNav.js
+++ linkedin_frontend/src/components/company_navbar/CompanyNav.js
@@ -1,8 +1,11 @@
 import "./companynav.css"
+import small_logo from "../../assets/small_logo.png"
 
 const CompanyNav = () => {
     return (
-        <div>CompanyNav</div>
+        <div className="company_nav">
+            <img className="small-logo" src={small_logo} alt="logo"/>
+        </div>
     )
 }
 
