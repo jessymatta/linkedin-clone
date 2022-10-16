5e8c74e9ad873e65f53ea74a222346bf907ef6ae
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 12:08:06 +0300

    Frontend <> Users Signup : added and styled the signup input fields

--- linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
+++ linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
@@ -4,6 +4,17 @@ const SignupFormUsers = () => {
     return (
         <div className="signup-form-users">
             <p class="signup-title">Join the biggest professional community</p>
+
+            <form className="signup-form">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" required="required" placeholder="Enter your name"/>
+
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" required="required" placeholder="Enter your email"/>
+
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" required="required" placeholder="Password (8+ characters)"/>
+        </form>
         </div>
     )
 }
