0993d0ca304b31f91229784328b5da7310dd9378
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 07:08:08 +0300

    Backend <> Users Auth : added the auth middleware to the users routes

--- linkedin_server/routes/user.routes.js
+++ linkedin_server/routes/user.routes.js
@@ -1,7 +1,8 @@
 const {Router} = require('express');
 const {getAllJobs} = require('../controllers/UserController')
+const AuthMiddleware = require('../middlewares/AuthMiddleware');
 const router = Router();
 
-router.get('/', getAllJobs);
+router.get('/', AuthMiddleware, getAllJobs);
 
 module.exports = router;
