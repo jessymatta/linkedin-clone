a3c647c4cfa33ab84e02655357250133bdaee7f3
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 12:19:12 +0300

    Frontend <> Users Signup : added and styled the signup button and other navigation links

--- linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
+++ linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
@@ -6,17 +6,30 @@ const SignupFormUsers = () => {
             <p class="signup-title">Join the biggest professional community</p>
 
             <form className="signup-form">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" required="required" placeholder="Enter your name"/>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" required="required" placeholder="Enter your name" />
 
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" required="required" placeholder="Enter your email"/>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" required="required" placeholder="Enter your email" />
 
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required="required" placeholder="Password (8+ characters)"/>
-        </form>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" required="required" placeholder="Password (8+ characters)" />
+
+                <div className="action-btns">
+                    <button type="submit" className="submit-signup">Join</button>
+                </div>
+            </form>
+
+            <div className="nav-to-other-forms">
+                <p className="gray smaller-text">Already on LinkedIn? <span className="blue smaller-text">Sign in</span></p>
+                <p className="blue smaller-text">Company account?</p>
+
+            </div>
+
         </div>
     )
 }
 
 export default SignupFormUsers
+
+
