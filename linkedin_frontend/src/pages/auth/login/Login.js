41e2d521eb316eef94645fd466e11be22e875ac2
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 18:09:27 +0300

    Frontned <> Users Login: linked the login page

--- linkedin_frontend/src/pages/auth/login/Login.js
+++ linkedin_frontend/src/pages/auth/login/Login.js
@@ -19,68 +19,99 @@ function Login() {
     }, []);
 
 
-    
+    const login = async (e) => {
+        e.preventDefault();
 
-    const goToSignup = () =>{
+        const userObject = {
+            email: user,
+            password: password,
+        }
+
+        const type = user_type;
+        if (type === "Personal") {
+            //login for users
+            axios.post(`http://localhost:8000/auth/login/personal`, userObject).then((res) => {
+                console.log(res.data);
+                localStorage.setItem("token", res.data);
+                //naviagte
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+
+        else {
+            //login for companies
+            axios.post(`http://localhost:8000/auth/login/company`, userObject).then((res) => {
+                console.log(res.data);
+                localStorage.setItem("token", res.data);
+                //naviagte
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+
+    };
+
+    const goToSignup = () => {
         navigate("/");
     }
 
     return (
         <>
-        <LogoNav/>
-        <div className="login-page">
-            
-            <section>
-                <h1>Sign in</h1>
-                <p className="small-text">Stay tuned on your professional world</p>
-                <form onSubmit={login}>
-                    {/* Username input */}
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        ref={userRef}
-                        autoComplete="off"
-                        onChange={(e) => {
-                            setUser(e.target.value);
+            <LogoNav />
+            <div className="login-page">
+
+                <section>
+                    <h1>Sign in</h1>
+                    <p className="small-text">Stay tuned on your professional world</p>
+                    <form onSubmit={login}>
+                        {/* Username input */}
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => {
+                                setUser(e.target.value);
+                            }}
+                            value={user}
+                            required
+                            placeholder="Enter your email"
+                        />
+
+                        {/* Password input */}
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                            value={password}
+                            required
+                            placeholder="Enter your password"
+                        />
+
+                        {/* User Type */}
+                        <label for="user_type">Select account type</label>
+                        <select name="user_type" id="user_type" onChange={(e) => {
+                            setUserType(e.target.value);
                         }}
-                        value={user}
-                        required
-                        placeholder="Enter your email"
-                    />
+                            value={user_type}>
+                            <option selected="true" disabled="disabled">Select type</option>
+                            <option>Personal</option>
+                            <option>Company</option>
+                        </select>
 
-                    {/* Password input */}
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                        value={password}
-                        required
-                        placeholder="Enter your password"
-                    />
-
-                    {/* User Type */}
-                    <label for="user_type">Select account type</label>
-                    <select name="user_type" id="user_type" onChange={(e) => {
-                        setUserType(e.target.value);
-                    }}
-                        value={user_type}>
-                        <option selected="true" disabled="disabled">Select type</option>
-                        <option>Personal</option>
-                        <option>Company</option>
-                    </select>
-
-                    {/*Login button */}
-                    <button>Sign in </button>
-                    <p className="gray smaller-text">New to LinkedIn? <span className="blue smaller-text" onClick={goToSignup}>Join now</span></p>
-                </form>
-            </section>
-        </div>
+                        {/*Login button */}
+                        <button>Sign in </button>
+                        <p className="gray smaller-text">New to LinkedIn? <span className="blue smaller-text" onClick={goToSignup}>Join now</span></p>
+                    </form>
+                </section>
+            </div>
         </>
     )
 }
