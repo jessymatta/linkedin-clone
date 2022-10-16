ccea36a45bae8a9e5ec9bbdf9b99be7e8e46c0e0
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 11:42:44 +0300

    Frontend <> Users Singup : added the main image in the signup page

---
+++ linkedin_frontend/src/components/sigup-img/SignupImg.js
@@ -0,0 +1,12 @@
+import "./signup-img.css"
+import signup_pic from "../../assets/signup_pic.PNG"
+
+const SignupImg = () => {
+    return (
+        <div class="signup-img">
+            <img src={signup_pic} alt="signup-main-img" />
+        </div>
+    )
+}
+
+export default SignupImg
--- linkedin_frontend/src/pages/auth/SignupUsers.js
+++ linkedin_frontend/src/pages/auth/SignupUsers.js
@@ -1,10 +1,12 @@
 import "./signup.users.css";
 import LogoNav from "../../components/logo_nav/LogoNav";
+import SignupImg from "../../components/sigup-img/SignupImg";
 
 const SignupUsers = () => {
     return (
         <div class="users-signup">
             <LogoNav/>
+            <SignupImg/>
         </div>
     )
 }
