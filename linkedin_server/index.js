0706ecce97ef2277e1d0a4a2f07647bbad85357c
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 06:04:58 +0300

    Backend <> Users : imported the users routes to index.js

--- linkedin_server/index.js
+++ linkedin_server/index.js
@@ -5,7 +5,10 @@ app.use(express.json());
 require('./config/db.config');
 
 const authRoutes = require('./routes/auth.routes');
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
+
+const userRoutes = require('./routes/user.routes');
+app.use('/users', userRoutes);
 
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
