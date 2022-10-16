d292ce5a1c59e248bb4b0b6a4afe5bc5cb521fbb
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 20:30:13 +0300

    Frontend <> Company profile: styled the company profile component

--- linkedin_frontend/src/components/profile/CompanyProfile.js
+++ linkedin_frontend/src/components/profile/CompanyProfile.js
@@ -1,8 +1,17 @@
 import "./company_profile.css"
+import gray from "../../assets/gray.png"
 
-const CompanyProfile = () => {
+//props.name, props.tagline, props.tagline
+const CompanyProfile = (props) => {
     return (
-        <div>CompanyProfile</div>
+        <div className="profile">
+            <div className="banner"></div>
+            <img  className="pp" src={gray} alt="logo"/>
+            <h3 className="welcome">Welcome, {props.name} !</h3>
+            <h9 className="tagline">{props.tagline}</h9> <br/>
+            <h9 className="location">{props.location}</h9>
+
+        </div>
     )
 }
 
--- linkedin_frontend/src/pages/company_pages/CompanyHomepage.js
+++ linkedin_frontend/src/pages/company_pages/CompanyHomepage.js
@@ -1,11 +1,12 @@
 import "./company_homepage.css"
 import CompanyNav from "../../components/company_navbar/CompanyNav"
+import CompanyProfile from "../../components/profile/CompanyProfile"
 
 const CompanyHomepage = () => {
     return (
         <div className="company-homepage">
             <CompanyNav />
-            CompanyHomepage
+            <CompanyProfile logo_url={"../../assets/gray.png"} name={"com"} tagline={"current tagline"} location={"location"}/>
         </div>
     )
 }
