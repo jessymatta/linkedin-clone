9097189591c23c6bff754feaf5d6fc057e952a01
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 11:54:07 +0300

    Frontend <> Users Signup : added and styled the title of the signup page

--- linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
+++ linkedin_frontend/src/components/signup-form-users/SignupFormUsers.js
@@ -2,7 +2,9 @@ import "./signup-form-users.css"
 
 const SignupFormUsers = () => {
     return (
-        <div className="signup-form-users">SignupFormUsers</div>
+        <div className="signup-form-users">
+            <p class="signup-title">Join the biggest professional community</p>
+        </div>
     )
 }
 
