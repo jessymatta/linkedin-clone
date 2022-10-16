7674d19aeebf593ece45c59956734fe1ca7caba9
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 19:15:14 +0300

    Frontend <> Company Homepage: added the nacbar component

---
+++ linkedin_frontend/src/components/company_navbar/CompanyNav.js
@@ -0,0 +1,9 @@
+import "./companynav.css"
+
+const CompanyNav = () => {
+    return (
+        <div>CompanyNav</div>
+    )
+}
+
+export default CompanyNav
---
+++ linkedin_frontend/src/pages/company_pages/CompanyHomepage.js
@@ -0,0 +1,13 @@
+import "./company_homepage.css"
+import CompanyNav from "../../components/company_navbar/CompanyNav"
+
+const CompanyHomepage = () => {
+    return (
+        <div>
+            <CompanyNav />
+            CompanyHomepage
+        </div>
+    )
+}
+
+export default CompanyHomepage
