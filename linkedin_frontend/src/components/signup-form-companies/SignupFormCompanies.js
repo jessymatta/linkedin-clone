2aebf831ee034eef21d0642669078c6f2f50b20c
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 15:49:51 +0300

    Frontend <> Signup Companies: initialized the signup page components for companies

---
+++ linkedin_frontend/src/components/signup-form-companies/SignupFormCompanies.js
@@ -0,0 +1,9 @@
+import "./signup-form-users.css"
+
+const SignupFormCompanies = () => {
+    return (
+        <div>SignupFormCompanies</div>
+    )
+}
+
+export default SignupFormCompanies
--- linkedin_frontend/src/pages/auth/SignupCompanies.js
+++ linkedin_frontend/src/pages/auth/SignupCompanies.js
@@ -1,8 +1,17 @@
 import "./signup.company.css";
+import LogoNav from "../../components/logo_nav/LogoNav";
+import SignupImg from "../../components/sigup-img/SignupImg";
+import SignupFormCompanies from "../../components/signup-form-companies/SignupFormCompanies";
 
 const SignupCompanies = () => {
     return (
-        <div>SignupCompanies</div>
+        <div className="users-signup">
+            <LogoNav />
+            <div className="signup-section">
+                <SignupFormCompanies />
+                <SignupImg />
+            </div>
+        </div>
     )
 }
 
