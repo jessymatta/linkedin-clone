eef1e42eac40d3602939f01dec63990c094ae2cb
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 15:15:46 +0300

    Backend <> added cors policies

--- linkedin_server/index.js
+++ linkedin_server/index.js
@@ -4,6 +4,16 @@ const app = express();
 app.use(express.json());
 require('./config/db.config');
 
+const cors = require('cors');
+app.use(
+    cors({
+        credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        origin: ['http://localhost:8000', 'http://localhost:3000'],
+    })
+);
+
 const authRoutes = require('./routes/auth.routes');
 app.use('/auth', authRoutes);
 
@@ -13,7 +23,7 @@ app.use('/users', userRoutes);
 const companyRoutes = require('./routes/company.routes');
 app.use('/companies', companyRoutes);
 
-app.listen(process.env.PORT, (err)=>{
-    if(err) throw err;
+app.listen(process.env.PORT, (err) => {
+    if (err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
 })
