a8a3bdcfba05f4ffefde555829cc9b0e856a3b9f
jessymatta <jessy.w.matta@gmail.com>
2022-10-16 11:02:52 +0300

    Frontend <> Refactoring and adding signup pages

--- linkedin_frontend/src/App.js
+++ linkedin_frontend/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <BrowserRouter>
+        <Routes>
+
+          <Route path="/">
+
+            <Route index element={<Login />} />
+
+            <Route path="admin">
+              <Route index element={<AdminDashboardInstructors />} />
+              <Route path="students" element={<AdminDashboardStudents />} />
+              <Route path="courses" element={<AdminDashboardCourses />} />
+              <Route path="add_instructor" element={<AdminAddInstructor/>} />
+              <Route path="add_student" element={<AdminAddStudent/>} />
+              <Route path="add_course" element={<AdminAddCourse/>} />
+            </Route>
+
+            <Route path="instructor">
+              <Route index element={<InstructorDashboardStudents />} />
+              <Route path="add_student" element={<InstructorAddStudent/>} />
+              <Route path="all_assignments" element={<InstructorAllAssign/>} />
+              <Route path="add_assignment" element={<InstructorAddAssign/>} />
+
+              <Route path="all_announcements" element={<InstructorAllAnnoun/>} />
+
+              <Route path="add_announcements" element={<InstructorAddAnnouncement/>} />
+            </Route>
+
+            <Route path="student">
+              <Route index element={<StudentDashboard />} />
+            </Route>
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
