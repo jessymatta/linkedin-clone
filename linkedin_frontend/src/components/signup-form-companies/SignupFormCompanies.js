7e4fe0dd8ea78854e1badddc93ebb8faba6ff3eb
jessymatta <jessy.w.matta@gmail.com>
2022-10-17 10:42:39 +0300

    Frontend <> Companies Signup: removed the unsupported 'selected' value and added values to options tags

--- linkedin_frontend/src/components/signup-form-companies/SignupFormCompanies.js
+++ linkedin_frontend/src/components/signup-form-companies/SignupFormCompanies.js
@@ -22,38 +22,40 @@ const SignupFormCompanies = () => {
             name: name,
             email: email,
             password: password,
-            size:size,
-            type:type,
-            tagline:tagline,
-            logo_url:logo_url
+            size: size,
+            type: type,
+            tagline: tagline,
+            logo_url: logo_url
         }
 
-        axios.post(`http://localhost:8000/auth/signup/company`, userObject).then((res) => {
+        axios.post(`http://localhost:8000/auth/signup/company`, userObject
+        ).then((res) => {
             console.log(res.data);
+            navigate("/company");
         }).catch((error) => {
             console.log(error);
         })
 
     };
 
-    const userSignup = () =>{
+    const userSignup = () => {
         navigate("/");
     }
 
-    const convertImage =(e)=>{
+    const convertImage = (e) => {
         let image_file = e.target.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(image_file);
+        let reader = new FileReader();
+        reader.readAsDataURL(image_file);
 
-    reader.onload = (e) => {
-        let image_url = e.target.result;
-        setLogo(image_url.split(',')[1])
+        reader.onload = (e) => {
+            let image_url = e.target.result;
+            setLogo(image_url.split(',')[1])
+        }
     }
-}
 
-const Login = () => {
-    navigate("/login");
-}
+    const Login = () => {
+        navigate("/login");
+    }
 
     return (
         <div className="signup-form-users">
@@ -74,36 +76,36 @@ const Login = () => {
                     }}
                     value={email} />
                 {/* Size */}
-                <label for="size">Organization size</label>
+                <label htmlFor="size">Organization size</label>
                 <select name="size" id="size" onChange={(e) => {
                     setSize(e.target.value);
                 }}
                     value={size}>
-                    <option selected="true" disabled="disabled">Select size</option>
-                    <option>0-1 employees</option>
-                    <option>2-10 employees</option>
-                    <option>11-50 employees</option>
-                    <option>51-200 employees</option>
-                    <option>201-500 employees</option>
-                    <option>501-1,000 employees</option>
-                    <option>1,001-5,000 employees</option>
-                    <option>5,001-10,000 employees</option>
-                    <option>10,000+ employees</option>
+                    <option value="DEFAULT" disabled="disabled">Select size</option>
+                    <option value="11">0-1 employees</option>
+                    <option value="22">2-10 employees</option>
+                    <option value="33">11-50 employees</option>
+                    <option value="44">51-200 employees</option>
+                    <option value="55">201-500 employees</option>
+                    <option value="66">501-1,000 employees</option>
+                    <option value="77">1,001-5,000 employees</option>
+                    <option value="88">5,001-10,000 employees</option>
+                    <option value="99">10,000+ employees</option>
                 </select>
                 {/* Type */}
-                <label for="type">Organization type</label>
+                <label htmlFor="type">Organization type</label>
                 <select name="type" id="type" onChange={(e) => {
                     setType(e.target.value);
                 }}
                     value={type}>
-                    <option selected="true" disabled="disabled">Select type</option>
-                    <option>Public company</option>
-                    <option>Self-employed</option>
-                    <option>Government agency</option>
-                    <option>Nonprofit</option>
-                    <option>Sole proprietorship</option>
-                    <option>Privetly Held</option>
-                    <option>Partnership</option>
+                    <option value="DEFAULT" disabled="disabled">Select type</option>
+                    <option value="1">Public company</option>
+                    <option value="2">Self-employed</option>
+                    <option value="3">Government agency</option>
+                    <option value="4">Nonprofit</option>
+                    <option value="5">Sole proprietorship</option>
+                    <option value="6">Privetly Held</option>
+                    <option value="7">Partnership</option>
                 </select>
 
                 {/* Tagline */}
@@ -113,7 +115,7 @@ const Login = () => {
                         setTagline(e.target.value);
                     }}
                     value={tagline} />
-                
+
 
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" required="required" placeholder="Password (8+ characters)"
