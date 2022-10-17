c4afaf386edf860d02f19e6838898d1a55afbbf8
jessymatta <jessy.w.matta@gmail.com>
2022-10-17 10:53:04 +0300

    Frontend <> Companies logout: added the logout feature

--- linkedin_frontend/src/components/company_navbar/CompanyNav.js
+++ linkedin_frontend/src/components/company_navbar/CompanyNav.js
@@ -4,15 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const CompanyNav = () => {
+    const navigate = useNavigate();
+
+    const logout = () =>{
+        localStorage.clear();
+        navigate("/login")
+
+    }
     return (
         <div className="company_nav">
             <img className="small-logo" src={small_logo} alt="logo"/>
             <div className="actions">
             <FontAwesomeIcon className="sidebar-icon" icon={faSuitcase}/>
             <FontAwesomeIcon className="sidebar-icon" icon={faUser}/>
-            <FontAwesomeIcon className="sidebar-icon" icon={faRightFromBracket}/>
+            <FontAwesomeIcon onClick={logout} className="sidebar-icon" icon={faRightFromBracket}/>
             </div>
             
         </div>
